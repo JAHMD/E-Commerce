1009a550d5f72d1f0277244db64b82a09986cc0d
Ahmed Wael <awael3977@gmail.com>
2023-07-16 11:58:34 +0300

    Fix not-found page and add its content

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
-	return <div>NotFound</div>;
+	return (
+		<section className="text-center text-primary-main-headdings">
+			<p className="font-semibold">404</p>
+			<h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
+				Page not found
+			</h1>
+			<p className="mt-6 text-base leading-7 text-primary-text">
+				Sorry, we couldn’t find the page you’re looking for.
+			</p>
+			<div className="mt-10 flex items-center justify-center gap-x-6">
+				<Link
+					to="/"
+					className="btn btn-primary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-header"
+				>
+					Go back home
+				</Link>
+			</div>
+		</section>
+	);
 };
 
 export default NotFound;
